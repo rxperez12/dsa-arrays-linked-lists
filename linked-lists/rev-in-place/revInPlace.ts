cf749528fc9338b38c61e10bfb44dc3f5de2915b
Ricardo Perez <rxperez12@gmail.com>
2024-07-08 17:32:36 -0700

    Add revInPlace function

--- linked-lists/rev-in-place/revInPlace.ts
+++ linked-lists/rev-in-place/revInPlace.ts
@@ -1,9 +1,22 @@
+import { ListFormat } from "typescript";
 import { LLStr, LLNodeStr } from "../common/ll";
 
 
 /** reverseInPlace() reverse list in place.*/
 
 function reverseInPlace(lst: LLStr): void {
+  let startIdx = 0
+  let endIdx = lst.length - 1
+
+  while(startIdx < lst.length/2){
+    const frontVal = lst.getAt(startIdx)
+    const backVal = lst.getAt(endIdx)
+
+    lst.setAt(startIdx, backVal)
+    lst.setAt(endIdx, frontVal)
+    startIdx++
+    endIdx--
+  }
 }
 
 
